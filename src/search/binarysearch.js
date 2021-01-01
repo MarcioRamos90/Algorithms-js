9c083411bab566fdc1b4326d8c8630b535844890
Márcio Ramos <marcioalvesramos90@hotmail.com>
2021-01-01 10:47:27 -0300

    refactor: split conditions and result object in funtions

--- src/search/binarysearch.js
+++ src/search/binarysearch.js
@@ -12,17 +12,33 @@ class BinarySearch {
     if (this.min >= this.max) {
       return undefined
     }
-    if (this.array[this.guess] === this.numToFind) {
-      return {
-        num: this.array[this.guess],
-        position: this.guess,
-      }
-    } else if (this.array[this.guess] < this.numToFind) {
+    if (this.isEqual(this.array[this.guess], this.numToFind)) {
+      return this.resultObject()
+    } else if (this.isLesser(this.array[this.guess], this.numToFind)) {
       this.min = this.guess + 1
-    } else if (this.array[this.guess] > this.numToFind) {
+    } else if (this.isGreater(this.array[this.guess], this.numToFind)) {
       this.max = this.guess - 1
     } 
     return this.run()
+  }
+
+  resultObject() {
+    return {
+      num: this.array[this.guess],
+      position: this.guess,
+    }
+  }
+
+  isEqual(value1, value2) {
+    return value1 === value2
+  }
+
+  isGreater(value1, value2) {
+    return value1 > value2
+  }
+
+  isLesser(value1, value2) {
+    return value1 < value2
   }
 
   calcGuess() {
